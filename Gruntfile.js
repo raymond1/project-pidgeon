f6667d0d11a1e2b318da03d628733586ca5e5906
Raymond Leon <raymondleon@raymondleon.ca>
2019-08-13 17:52:53 -0400

    Better rudimentary gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,14 @@
 module.exports = function(grunt) {
-
+    grunt.initConfig(
+        {
+            copy: {
+                main:{
+                    src: 'node_modules/jquery/dist/jquery.js',
+                    dest: 'dist/jquery.js',
+                }    
+            },
+        }
+    );
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['copy'])
 };
